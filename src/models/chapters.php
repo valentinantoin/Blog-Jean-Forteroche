93ba6269b639e55a407fbf34ba3e9e9ac4f7e099
Valentin <valentin.antoin@gmail.com>
2019-05-01 17:38:08 +0200

    Add chapters imports

---
+++ src/models/chapters.php
@@ -0,0 +1,11 @@
+<?
+
+function chapters() {
+
+$pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$chapters = $pdo->query('SELECT * FROM chapters ORDER BY id DESC LIMIT 3');
+
+return $chapters;
+}
