ce383bb6840ac3bf0ab77935df5e48c2e7d2830f
Valentin <valentin.antoin@gmail.com>
2019-05-02 12:45:19 +0200

    Add routing to all .twig

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@ $loader = new Twig_Loader_Filesystem(__DIR__. '../../src/views');
 
 
 
-function chapters() {
+function chaptersHome() {
 
     $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -30,18 +30,47 @@ function chapters() {
     return $chapters;
 }
 
+function chaptersPage() {
+
+    $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $chapters = $pdo->query('SELECT * FROM chapters ORDER BY id ASC');
+
+    return $chapters;
+}
+
+
+
+
 switch ($page) {
 
     case "home" :
-        echo $twig->render("home.twig", ['chapters' => chapters()]);
+        echo $twig->render("home.twig", ['chapters' => chaptersHome()]);
         break;
 
     case "contact" :
         echo $twig->render("contact.twig");
         break;
 
+    case "chapters" :
+        echo $twig->render("chapters.twig", ['chapters' => chaptersPage()]);
+        break;
+
+    case "presentation" :
+        echo $twig->render("presentation.twig");
+        break;
+
+    case "connection" :
+        echo $twig->render("connection.twig");
+        break;
+
+    case "subscribe" :
+        echo $twig->render("subscribe.twig");
+        break;
+
     default:
-        echo $twig->render("home.twig", ['chapters' => chapters()]);
+        echo $twig->render("home.twig", ['chapters' => chaptersHome()]);
         break;
 }
 
