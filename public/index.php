265b20a3cf12baf7af3564ad19141430e62d3af3
Valentin <valentin.antoin@gmail.com>
2019-05-02 13:17:24 +0200

    Delete routing process

--- public/index.php
+++ public/index.php
@@ -2,23 +2,6 @@
 
 require "../vendor/autoload.php";
 
-//ROUTING
-$page = 'home';
-
-    if(isset($_GET['acces'])) {
-
-        $page = $_GET['acces'];
-}
-
-
-//TEMPLATE
-
-$loader = new Twig_Loader_Filesystem(__DIR__. '../../src/views');
-    $twig = new Twig_Environment($loader, [
-        'cache' => false
-    ]);
-
-
 
 function chaptersHome() {
 
@@ -41,36 +24,4 @@ function chaptersPage() {
 }
 
 
-
-
-switch ($page) {
-
-    case "home" :
-        echo $twig->render("home.twig", ['chapters' => chaptersHome()]);
-        break;
-
-    case "contact" :
-        echo $twig->render("contact.twig");
-        break;
-
-    case "chapters" :
-        echo $twig->render("chapters.twig", ['chapters' => chaptersPage()]);
-        break;
-
-    case "presentation" :
-        echo $twig->render("presentation.twig");
-        break;
-
-    case "connection" :
-        echo $twig->render("connection.twig");
-        break;
-
-    case "subscribe" :
-        echo $twig->render("subscribe.twig");
-        break;
-
-    default:
-        echo $twig->render("home.twig", ['chapters' => chaptersHome()]);
-        break;
-}
-
+require ("../src/controllers/frontController.php");
