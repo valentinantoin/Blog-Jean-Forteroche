c9dcc39b0d68adc61b889d3757f6cb16e5720b75
Valentin <valentin.antoin@gmail.com>
2019-05-02 18:17:12 +0200

    Create chapter prototype and CRUD methods

--- src/models/chapters.php
+++ src/models/chapters.php
@@ -1,24 +1,61 @@
 <?php
 
-//SHOW LAST CHAPTER ON HOME PAGE
-function chaptersHome() {
+//CREATE PROTOTYPE
+class ChapterManager {
 
-    $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    $chapters = $pdo->query('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr FROM chapters ORDER BY id DESC LIMIT 1');
+//CREATE CHAPTER
+    public function addChapter($id, $title, $content) {
 
-    return $chapters;
+        $pdo = $this->dbConnect();
+        $newChapter = $pdo->prepare('INSERT INTO chapters(id, title, content, creation_date) VALUES(?, ?, ?, NOW())');
+        $affectedLines = $newChapter->execute(array($id, $title, $content));
+
+        return $affectedLines;
+    }
+
+//READ CHAPTER
+    public function getChapterHome() {
+
+        $pdo = $this->dbConnect();
+        $lastChapter = $pdo->query('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr FROM chapters ORDER BY id DESC LIMIT 1');
+
+        return $lastChapter;
+    }
+
+    public function getChapterPage() {
+
+        $pdo = $this->dbConnect();
+        $chapters = $pdo->query('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y \') AS creation_date_fr FROM chapters ORDER BY id ASC');
+
+        return $chapters;
+    }
+
+//UPDATE CHAPTER
+    public function updateChapter() {
+
+        $pdo = $this->dbConnect();
+        $req = $pdo->prepare('UPDATE chapters SET content = :new_content, creation_date =:new_date WHERE titlte =:title');
+        $req->execute(array('new_content' => $new_content,
+                             'new_date' => $NOW(),
+                             'title' => $title
+                            ));
+    }
+
+//DELETE CHAPTER
+    public function deleteChapter() {
+        $pdo = $this->dbConnect();
+        $req = $pdo->prepare('DELETE FROM chapters WHERE title= :title');
+        $req->execute(array('title' => $title));
+    }
+
+
+//CONNECT TO DB
+    private function dbConnect(){
+
+        $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+        return $pdo;
+    }
 }
-
-//SHOW EVERY CHAPTER ON CHAPTER PAGE
-function chaptersPage() {
-
-    $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    $chapters = $pdo->query('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y \') AS creation_date_fr FROM chapters ORDER BY id ASC');
-
-    return $chapters;
-}
-
