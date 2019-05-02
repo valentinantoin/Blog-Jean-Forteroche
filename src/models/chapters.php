e80ab811259e1aa9ea79c23ddc7ae47bf8db542c
Valentin <valentin.antoin@gmail.com>
2019-05-02 13:18:19 +0200

    Add read function

--- src/models/chapters.php
+++ src/models/chapters.php
@@ -1,1 +1,22 @@
 <?
+
+
+function chaptersHome() {
+
+    $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $chapters = $pdo->query('SELECT * FROM chapters ORDER BY id DESC LIMIT 3');
+
+    return $chaptersHome;
+}
+
+function chaptersPage() {
+
+    $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $chapters = $pdo->query('SELECT * FROM chapters ORDER BY id ASC');
+
+    return $chaptersPage;
+}
