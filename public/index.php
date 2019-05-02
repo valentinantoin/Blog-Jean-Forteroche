38c789cc21c6031c0c61fe1b97a9d097269f12e6
Valentin <valentin.antoin@gmail.com>
2019-05-02 18:21:55 +0200

    Delete chapters.php loading

--- public/index.php
+++ public/index.php
@@ -3,8 +3,5 @@
 //autoload
 require "../vendor/autoload.php";
 
-//load chapters model
-require('../src/models/chapters.php');
-
 //load front controller for routing
 require ("../src/controllers/frontController.php");
