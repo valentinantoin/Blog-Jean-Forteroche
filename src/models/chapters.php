807f3d574a090a476fb0f87d6d3462a858c5c559
Valentin <valentin.antoin@gmail.com>
2019-05-02 16:49:19 +0200

    Fix request syntax

--- src/models/chapters.php
+++ src/models/chapters.php
@@ -1,22 +1,24 @@
-<?
+<?php
 
-
+//SHOW LAST CHAPTER ON HOME PAGE
 function chaptersHome() {
 
     $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    $chapters = $pdo->query('SELECT * FROM chapters ORDER BY id DESC LIMIT 3');
+    $chapters = $pdo->query('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr FROM chapters ORDER BY id DESC LIMIT 1');
 
-    return $chaptersHome;
+    return $chapters;
 }
 
+//SHOW EVERY CHAPTER ON CHAPTER PAGE
 function chaptersPage() {
 
     $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    $chapters = $pdo->query('SELECT * FROM chapters ORDER BY id ASC');
+    $chapters = $pdo->query('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y \') AS creation_date_fr FROM chapters ORDER BY id ASC');
 
-    return $chaptersPage;
+    return $chapters;
 }
+
