4729209dd824f585a46b56f78493e72118d51b9f
Valentin <valentin.antoin@gmail.com>
2019-05-13 14:29:18 +0200

    User connection test

--- src/controllers/connectionController.php
+++ src/controllers/connectionController.php
@@ -1,3 +1,24 @@
 <?php
 
-echo $twig->render("connection.twig");
+use App\models\UserManager;
+require ('../models/UserManager.php');
+
+$pseudo = $_POST['pseudo'];
+$pass = $_POST['pass'];
+$hashPass = password_hash($pass, PASSWORD_DEFAULT);
+
+
+$userManager = new UserManager();
+$userPass = $userManager->getUser($pseudo);
+
+if ($hashPass == $userPass ){
+
+    session_start();
+    $_SESSION['pseudo'] = $pseudo;
+
+
+    header('Location: ../../public/index.php');
+}else {
+
+    echo "Mot de passe incorrect.";
+}
