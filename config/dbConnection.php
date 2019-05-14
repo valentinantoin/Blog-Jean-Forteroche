eed008a8915bfac7b584e32cd8282fdbdce67c11
Valentin <valentin.antoin@gmail.com>
2019-05-14 10:48:02 +0200

    Update online version

--- config/dbConnection.php
+++ config/dbConnection.php
@@ -10,7 +10,7 @@ class DbConnection
     public function dbConnect()
     {
 
-        $pdo = new PDO('mysql:dbname=jeanforteroche;host=localhost', 'root', '');
+        $pdo = new PDO('mysql:dbname=anva6816_jeanforteroche;host=localhost', 'anva6816_jeanforteroche', 'jeanjean');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
