caaed14e3ca873b7591a3739c82712551f714a54
Valentin <valentin.antoin@gmail.com>
2019-05-15 14:13:42 +0200

    Add redirections

--- src/controllers/frontController.php
+++ src/controllers/frontController.php
@@ -2,10 +2,15 @@
 
 
 //TEMPLATES PREPARATION
-$loader = new Twig_Loader_Filesystem( '../src/views');
+$loader = new Twig_Loader_Filesystem( '..\src\views');
 $twig = new Twig_Environment($loader, [
     'cache' => false,
+    'debug' => true,
 ]);
+$twig->addGlobal('session', $_SESSION);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+$filter = new \Twig\TwigFilter('nl2br', 'nl2br', ['is_safe' => ['html']]);
+$twig->addFilter($filter);
 
 
 //ROUTING
@@ -27,14 +32,14 @@ switch ($page) {
     case "chapters" :
         require ("ChapterController.php");
         $chapterController = new ChapterController($twig);
-        $chapterLast = $chapterController->chapterList();
+        $chapterList = $chapterController->chapterList();
         break;
 
     case "chapter" :
         require ("ChapterController.php");
         require ("CommentController.php");
         $chapterController = new ChapterController($twig);
-        $chapterLast = $chapterController->chapterRead();
+        $chapterRead = $chapterController->chapterRead();
         break;
 
     case "presentation" :
@@ -45,15 +50,41 @@ switch ($page) {
         require('connectionView.php');
         break;
 
+    case "disconnect" :
+        session_destroy();
+        header('Location: ../public/index.php');
+        break;
+
     case "subscribe" :
         require ('subscribeController.php');
         break;
 
+   case "postsubscribe" :
+        require ('UserController.php');
+        $userController = new UserController($twig);
+        $addUser = $userController->subscribe();
+        break;
+
+    case "connected" :
+        require ('UserController.php');
+        $userController = new UserController($twig);
+        $addUser = $userController->connection();
+        break;
+
+    case "postchap" :
+        require ('ChapterController.php');
+        $chapterController = new ChapterController($twig);
+        $addChapter = $chapterController->chapterAdd();
+        break;
+
+    case "postcom" :
+        require ('CommentController.php');
+        $commentController = new CommentController($twig);
+        $addComment = $commentController->addComment();
+        break;
+
     case "admin" :
         require ('adminController.php');
-        //require ('ChapterController.php');
-        //$chapterController = new ChapterController($twig);
-        //$chapterLast = $chapterController->chapterAdd();
         break;
 
     default:
