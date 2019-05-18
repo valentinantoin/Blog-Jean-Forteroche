5c5b74048baaa736f7dbe0a0b95a021c4cf5965f
Valentin <valentin.antoin@gmail.com>
2019-05-18 11:22:34 +0200

    Create class DbConnection

--- config/dbConnectionExample.php
+++ config/dbConnectionExample.php
@@ -1,11 +1,19 @@
 <?php
 
-function dbConnect() {
+namespace config;
 
-    $pdo = new PDO('mysql:dbname=example ;host=example', 'example', 'example');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+use \PDO;
 
-    return $pdo;
 
+class DbConnectionExample {
+
+    protected $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = new PDO('mysql:host=localhost;dbname=example;charset=utf8', 'example', 'example');
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+    }
 }
