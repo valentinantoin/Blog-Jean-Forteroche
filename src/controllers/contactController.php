ee1a7bea771a9213a5d34abe2c46fa26130ad60f
Valentin <valentin.antoin@gmail.com>
2019-05-21 15:22:53 +0200

    Create ContactController class

--- src/controllers/contactController.php
+++ src/controllers/contactController.php
@@ -1,21 +1,37 @@
-
-<!doctype html>
-<html lang="fr">
-
-<head>
-    <meta charset="utf-8">
-    <title>Envoi d'un email par formulaire</title>
-</head>
-
-<body>
 <?php
-$retour = mail('valentin.antoin@gmail.com', $_POST['title'] ,$_POST['name'], $_POST['message'], $_POST['mail']);
-if ($retour) {
-    echo "<script>alert(\"Votre message est bien envoyé \")</script>";
 
-    echo $twig->render('contact.twig');
+use App\Controllers\Controller;
+
+
+/**
+ * Class ContactController
+ */
+class ContactController extends Controller
+{
+    /**
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function sendMail()
+    {
+        $name = htmlentities($_POST['name']);
+        $mail = htmlentities($_POST['mail']);
+        $title = htmlentities($_POST['title']);
+        $content = htmlentities($_POST['message']);
+
+        $to = "valentin.antoin@gmail.com";
+        $subject = $title;
+        $message = $content;
+
+        $header = 'MIME-Version: 1.0'."\r\n";
+        $header .= 'Content-type: text/html; charset=utf-8'."\r\n";
+        $header .= 'From: '.$name.' <'.$mail.'>'."\r\n";
+
+        mail($to,$subject,$message, $header);
+
+        echo "<script>alert(\"Votre mail est bien envoyé\")</script>";
+
+        echo $this->render('contact.twig');
+    }
 }
-?>
-</body>
-
-</html>
